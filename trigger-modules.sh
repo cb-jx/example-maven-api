84d60d0c31e951c9fb6cd28c5bbf1b521b878b6e
caternberg <caternberg@gm.de>
2014-11-19 21:52:24 +0100

    trigger jenkins build

--- trigger-modules.sh
+++ trigger-modules.sh
@@ -3,4 +3,4 @@ MODULES=( impl api )
 for module in $MODULES;do
 echo "Trigger" >> ${module}/TRIGGER.txt
 git add  ${module}/TRIGGER.txt;done
-git commit -m "trigger jenkins build" .
+git commit -m "trigger jenkins build" . && git push

7e9b9b8cc2bfc07add418e6b1ebfaec78760262f
caternberg <andreas.caternberg@gmail.com>
2017-06-26 20:49:44 +0200

    add junit

---
+++ impl/src/test/java/de/simple/impl/SimpleImplTest.java
@@ -0,0 +1,17 @@
+package de.simple.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by andi on 26.06.17.
+ */
+@Test
+public class SimpleImplTest {
+
+
+    public void testDoOne(){
+       assertTrue(true,true)
+    }
+}
